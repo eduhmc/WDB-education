2c489e52eada644a78a5db4b046ea7935aa77121
eduhmc <eduhmc@berkeley.edu>
2021-10-26 13:11:35 -0700

    hw4

---
+++ hw4/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//The line above imports all the necessary css files similar to the way done in html. Done
+//Imports in React are neccessary whenever you want to reference an outside file or library.
+import Feed from './Components/Feed'
+import Navigbar from '/Users/eduardohuertamercado/Desktop/fullStack/hw4/src/Components/Navigbar';
+// Import Bootstrap Navigation Bar HERE. Done
+// Remember that it should be used as a React Component
+
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your Navigation Bar that you imported as component should be here, look at 
+      how the Feed Component is imported as a reference point.
+      Remember that React Components must start with a capital letter.*/}
+      <h1>Blockstagram</h1>
+      <Feed></Feed>
+    </div>
+  );
+}
+
+export default App;
---
+++ hw4/src/Components/Feed.js
@@ -0,0 +1,60 @@
+import React from 'react';
+// ADD CODE HERE
+import Block from './Block'
+
+class Feed extends React.Component {
+
+    state = {
+      // ADD CODE HERE
+      allblocks: [],
+      showopt: false
+    }
+
+    addBlock = (color) => {
+        this.setState({
+        // ADD CODE HERE
+        allblocks: [<Block color={color} />, ...this.state.allblocks]
+      })
+    }
+
+    pickColorlayout = () => {
+      if (this.state.showopt == false) {
+        this.setState({
+          showopt: true
+        })
+      }
+      else{
+        this.setState({
+            showopt: false
+        })
+      }
+    }
+    
+    render() {
+
+      return (
+        <div className = 'mainfeed'>
+        <div className="input">
+        {this.state.allblocks}
+        </div>
+        <div>
+          <button class="button" onClick={this.pickColorlayout}>+ Post Block</button>
+        </div>
+        { this.state.showopt ?         <div className="pickblock">
+        <div>
+            <button className="red"  onClick={() => this.addBlock("rgb(134, 5, 5)")}></button>
+            <button className="blue" onClick={() => this.addBlock("rgb(21, 0, 141)")}></button>
+        </div>
+        <div>
+            <button className="green"  onClick={() => this.addBlock("rgb(59, 110, 0)")}></button>
+            <button className="purple"  onClick={() => this.addBlock("rgb(58, 0, 97)")}></button>
+        </div>
+     </div>: null }   
+      </div>
+    
+      );
+    }
+  }
+
+
+  export default Feed;
